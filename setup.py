95ec40b194a65875a286b8ef697e766b0ca33cb4
sayboltm <sayboltm@users.noreply.github.com>
2017-09-29 17:06:41 -0400

    Completed rest of tutorial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='leet',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/sayboltm/leet',
+      author='Mike',
+      author_email='sayboltm@users.noreply.github.com',
+      license='MIT',
+      packages=['leet'],
+      zip_safe=False)
